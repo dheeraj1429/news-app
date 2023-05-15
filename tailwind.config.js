0ca807addb55d9f5802134559980cb0c399c4b71
dheeraj1429 <dheerajsingh1429@gmail.com>
2023-05-15 16:10:38 +0530

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
    content: [
-      './app/**/*.{js,ts,jsx,tsx,mdx}',
-      './pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-      // Or if using `src` directory:
-      './src/**/*.{js,ts,jsx,tsx,mdx}',
+      // using ./src/ dir
+      './src/**/*.{js,ts,jsx,tsx}',
+      // using ./ dir
+      './app/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+      // add more paths here
    ],
    theme: {
       extend: {},
